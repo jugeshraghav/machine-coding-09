cfdb507d03491f611eaa6260d25a54a323f0c921
jugesh raghav <74092377+jugeshraghav@users.noreply.github.com>
2023-07-29 11:02:07 +0530

    initial commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,45 @@
+import { useState } from "react";
 import { useData } from "../index";
+import { Box, Button, Modal, Typography } from "@mui/material";
 
 export const Home = () => {
   const { state } = useData();
+
+  const [openModal, setOpenModal] = useState(false);
+  const handleClose = () => {
+    setOpenModal(false);
+  };
+
   return (
     <>
       <h1>Home {state?.name}</h1>
+      <Button
+        color="success"
+        variant="contained"
+        onClick={() => setOpenModal(true)}
+      >
+        Open modal
+      </Button>
+      <Modal
+        open={openModal}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          sx={{
+            width: 300,
+            backgroundColor: "red",
+            color: "white",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+          }}
+        >
+          <Typography variant="h6"> Heading</Typography>
+          <Typography variant="p">Paragraph</Typography>
+        </Box>
+      </Modal>
     </>
   );
 };
